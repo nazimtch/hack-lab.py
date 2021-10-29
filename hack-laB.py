da53f594ecbdb89d015873a06fea589b6f894f91
NF-attacker <59114717+nazimtch@users.noreply.github.com>
2021-10-30 01:33:02 +0200

    Add files via upload

---
+++ hack-laB.py
@@ -0,0 +1,64 @@
+import os
+os.system ("clear")
+password = input ("ENTER THE PASSWORD TO USE THIS TOOL : ")
+if password == ("hashmd5tryagainn") :
+    print ("WELCOME TO NF-HACK-LAB")
+    print ("NF-HACK-LAB IS A POWERFUL | INTERNAL / EXTERNAL | HACK TOOL")
+    print ("CREATED BY NAZIMTCH | NF-ATTACKER ")
+    print ("TO USE ONLY ON LINUX OS")
+    print ("CREATED USING PYTHON3 LANGUAGE ")
+    print ("INSTAGRAM : https://www.instagram.com/nazimtch")
+    print ("SNAP : nazimtchh")
+else : 
+    print ("WRONG PASSWORD")
+    print ("CREATED BY NAZIMTCH TEAM | PLEASE CONTACT THE CODER")
+    print ("PLEASE CONTACT US WE WILL SEND YOU THE PASSWORD")
+    print ("INSTAGRAM : https://www.instagram.com/nazimtch")
+    import time
+    time.sleep(8)
+    os.system ("apt install bash")
+    os.system ("bash README.sh")
+ #user interface script username = input ("ENTER YOUR USERNAME : ")
+password2 = input ("PLEASE ENTER YOUR PASSWORD AGAIN : ")
+if password2 == ("hashmd5tryagainn") :
+    username = input ("PLEASE  ENTER ANY USERNAME TO USE  : ")
+    import time
+    time.sleep(3)
+    print ("HI USER " " " +username )
+    print ("ENJOY (: ")
+    time.sleep(3)
+    import os
+    os.system ("clear")
+#HACK-LAB SCRIPT
+    print ("OUR PENETRATION SERVICE")
+    print ("1 ------ : INFORMATION-GATHERING TOOLS")
+    print ("2 ------ : PASSWORDS-ATTACK")
+    print ("3 ------ : SERVER-ATTACK ")
+    print ("4 ------ : SQL-INJECTION ")
+    print ("5 ------ : DNS-ATTACK")
+    print ("6 ------ : SNIFFING-SPOFFING")
+    print ("7 ------ : WEB-HACKING")    
+    option = input ("PLEASE CHOOSE A SERVICE : ")
+    time.sleep(4)
+    os.system ("clear")
+#if choice script
+    if option == ("1") :
+        print ("A ------ : NF-IP-FOUNDER")
+        print ("B ------ :NF-GATHERING")
+        print ("C ------ :NF-GATHERING V 2.0")
+        print ("D ------ :NMAP")
+        choice = input ("CHOOSE A TOL TO USE : ")
+        if choice == ("A") :
+            os.system ("clear")
+            print ("NF-IP-FOUNDER IS A TOOL CREATED BY NAZIMTCH ")
+            time.sleep(2)
+            print ("THIS TOOL HELP YOU TO GET ANY IP OF ANNY WEBSITE")
+            print ("THE IP WILL APPEAR EXAMPLE : 192.168.1.1")
+            IP = input (" ENTER THE NAME OF THE WEBSITE : ")
+            os.system ("ping" " " +IP)
+        elif choice == ("B") :
+            import os
+            os.system ("apt install bash")
+            os.system ("python3 nf-gathering.py")    
+        elif choice == ("C") :
+            print ("THIS TOOL WILL BE UPLOAD SOON PLEASE CHECK OTHER SERVICE")
